ddf44de0ea3ba9ba9c305ff8807367919d5466e0
ikhsanhikari <ikhsanhikari29@gmail.com>
2023-03-29 10:54:36 +0700

    init

---
+++ src/main/java/com/dans/multi/pro/DansMultiProApplication.java
@@ -0,0 +1,13 @@
+package com.dans.multi.pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DansMultiProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DansMultiProApplication.class, args);
+	}
+
+}
